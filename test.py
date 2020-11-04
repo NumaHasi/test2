6b00cd0baf91c3485cba5c48029ac5e105e03862
Your Name <you@example.com>
2020-11-04 10:33:02 +0900

    {}

--- test.py
+++ test.py
@@ -1,19 +1,9 @@
 import os
-
-
-os.system("curl https://twitter.com/abeshinzo > test8.html")
-
+#os.system("curl http://abehiroshi.la.coocan.jp/ > test8.html")
 f = open("test8.html",'r')
-
 markdown = open("test.md",'w')
-
-
 #print(f.read() )
-
-
-markdown.write(f.read() )
-
-
+#markdown.write(f.read())
 markdown.close()
 f.close()
 
