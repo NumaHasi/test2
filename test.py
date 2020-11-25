9018ccbe02939f140610d185b53c3f05f9a3ea2e
NumaHasi <tomobeken@sika3.com>
2020-11-25 10:02:44 +0900

    {}

--- test.py
+++ test.py
@@ -4,9 +4,17 @@ os.system("curl http://192.168.0.100/ > test.txt")
 f = open("test.txt",'r')
 markdown = open("test.md",'w')
 
+markdown.write(f.read())
+markdown.close()
+f.close()
+
 os.system("curl http://192.168.0.100/ > test2.txt")
 f = open("test2.txt",'r')
 markdown = open("test2.md",'w')
+
+markdown.write(f.read())
+markdown.close()
+f.close()
 
 os.system("curl http://192.168.0.100/ > test3.txt")
 f = open("test3.txt",'r')
