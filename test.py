4a557797f28ccb12ac512f8a18fbebd7d531b2bf
NumaHasi <numarabo11@gmail.com>
2020-11-26 03:33:10 +0900

    {}

--- test.py
+++ test.py
@@ -27,18 +27,18 @@ for i in range(5):
     f.close()
     d.close()
     
-    if i>2:
-        
-        x=0
-        with open("water1") as f:
-            x=x+1
-            line=f.readline()
-            if x !=1:
-                o.write(line)
-        o.close()
-        
-        os.system("cp without.txt water1")
-        
+#     if i>5:
+#         
+#         x=0
+#         with open("water1") as f:
+#             x=x+1
+#             line=f.readline()
+#             if x !=1:
+#                 o.write(line)
+#         o.close()
+#         
+#         os.system("cp without.txt water1")
+#         
         
     f = open("test.txt",'r')
     c = open("kasoku.txt","w")
