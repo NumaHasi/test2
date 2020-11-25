8a8d5aa5a2fdc233690d6be33f9c1071271300e4
NumaHasi <tomobeken@sika3.com>
2020-11-25 10:15:07 +0900

    {}

--- test.py
+++ test.py
@@ -1,31 +1,36 @@
 import os
+import time
 
-os.system("curl http://192.168.0.100/ > test.txt")
-f = open("test.txt",'r')
-markdown = open("test.md",'w')
+for i in range(5):
+        
+    os.system("curl http://192.168.0.100/ > test.txt")
+    f = open("test.txt",'r')
+    markdown = open("test.md",'w')
 
-markdown.write(f.read())
-markdown.close()
-f.close()
+    markdown.write(f.read())
+    markdown.close()
+    f.close()
 
-os.system("curl http://192.168.0.100/ > test2.txt")
-f = open("test2.txt",'r')
-markdown = open("test2.md",'w')
+    os.system("curl http://192.168.0.101/ > test2.txt")
+    f = open("test2.txt",'r')
+    markdown = open("test2.md",'w')
 
-markdown.write(f.read())
-markdown.close()
-f.close()
+    markdown.write(f.read())
+    markdown.close()
+    f.close()
 
-os.system("curl http://192.168.0.100/ > test3.txt")
-f = open("test3.txt",'r')
-markdown = open("test3.md",'w')
+    os.system("curl http://192.168.0.100/ > test3.txt")
+    f = open("test3.txt",'r')
+    markdown = open("test3.md",'w')
 
-markdown.write(f.read())
-markdown.close()
-f.close()
+    markdown.write(f.read())
+    markdown.close()
+    f.close()
 
-os.system("git add .")
+    os.system("git add .")
 
-os.system("git commit -m {}")
+    os.system("git commit -m {}")
 
-os.system("git push")
+    os.system("git push")
+    
+    time.sleep(60)
